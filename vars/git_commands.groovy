13c59bfbf242b36f062d25ab0c138f35cc3f01d1
brajcic <bojanrajcic25@gmail.com>
2020-10-09 01:05:22 -0700

    Groovy git clone

--- vars/git_commands.groovy
+++ vars/git_commands.groovy
@@ -2,5 +2,6 @@
 void gitClone() {
     def git_clone1 = "git clone https://github.com/brki18/ansible_for_devops.git".execute()
     def git_clone2 = "git clone https://github.com/brajcic/groovy_test.git".execute()
+    def git_clone3 = "git clone https://github.com/brajcic/private_rep.git".execute()
 
 }
