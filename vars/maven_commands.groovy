f5e0947fd8eeccef0fb5e9ad07d2e9ccaae2c027
brajcic <bojanrajcic25@gmail.com>
2020-10-13 01:59:44 -0700

    Groovy git clone & branch

--- vars/maven_commands.groovy
+++ vars/maven_commands.groovy
@@ -1,3 +1,4 @@
 def  installMaven() {
-   println("mvn clean install".execute().toString())
+   //println("mvn clean install".execute().toString())
+   "cd /home/brajcic/IdeaProjects/maven_test && mvn clean install".execute()
 }
